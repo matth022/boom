6d87ebfc635e500acede0fdce9a67635a83963ec
matth022 <matthieu.hertoghe@student.unamur.be>
2017-08-22 15:11:12 +0200

    Update generate-toc.js

--- generate-toc.js
+++ generate-toc.js
@@ -2,30 +2,27 @@
 
 // Load modules
 
-const Toc = require('markdown-toc');
-const Fs = require('fs');
+var Toc = require('markdown-toc');
+var Fs = require('fs');
 
 // Declare internals
 
-const internals = {
+var internals = {
     filename: './README.md'
 };
 
-
 internals.generate = function () {
 
-    const api = Fs.readFileSync(internals.filename, 'utf8');
-    const tocOptions = {
+    var api = Fs.readFileSync(internals.filename, 'utf8');
+    var tocOptions = {
         bullets: '-',
-        slugify: function (text) {
+        slugify: function slugify(text) {
 
-            return text.toLowerCase()
-                .replace(/\s/g, '-')
-                .replace(/[^\w-]/g, '');
+            return text.toLowerCase().replace(/\s/g, '-').replace(/[^\w-]/g, '');
         }
     };
 
-    const output = Toc.insert(api, tocOptions);
+    var output = Toc.insert(api, tocOptions);
     Fs.writeFileSync(internals.filename, output);
 };
 
